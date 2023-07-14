7312660ba540c8b62a91dd70f998da02a4bb9435
FebryanS <49833582+FebryanShino@users.noreply.github.com>
2023-07-14 15:29:27 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ fetch('https://databaseapi.febryanshino.repl.co/')
     
     for(let i = 0; i < events.length; i++) {
       let event = events[i];
-      let eventID = event[0];
-      let eventTitle = event[1];
-      let eventDesc = event[2];
-      let eventURL = event[3];
-      let eventDate = event[4];
-      console.log(eventID);
+      let eventURL = event[1].trim(). toLowerCase();
+      let eventDesc = event[2].trim();
+      let eventDate = event[0].trim();
       
       const item = document.createElement('div');
       const icon = document.createElement('div');
@@ -29,7 +26,8 @@ fetch('https://databaseapi.febryanshino.repl.co/')
       desc.classList.add('desc');
     
       text.textContent = eventDesc;
-      link.href = eventURL;
+      icon.style.backgroundImage = `url(/events/${eventURL}/thumbnail.jpg)`;
+      link.href = `/events/${eventURL}`;
       link.textContent = 'Learn more';
       date.textContent = eventDate;
 
@@ -43,14 +41,46 @@ fetch('https://databaseapi.febryanshino.repl.co/')
   
     }
   });
+
+const phoneIcon = document.querySelector('#call');
+
+
+phoneIcon.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  let status = phoneIcon.getAttribute('data-status');
+  const contactContainer = document.querySelector('.contact');
+  const icon = phoneIcon.firstElementChild;
+
+  if(status === 'disabled') {
+    icon.style.transform = 'rotate(0deg)';
+    phoneIcon.style.filter = 'invert(100%)';
+    
+    contactContainer.style.maxHeight = '25rem';
+    phoneIcon.setAttribute('data-status','enabled');
     
 
+  } else {
+    icon.style.transform = 'rotate(135deg)';
+    phoneIcon.style.filter = 'invert(0%)';
+    
+    contactContainer.style.maxHeight = 0;
+    phoneIcon.setAttribute('data-status','disabled');
+  }
+});
 
 
 
 
+const feedBackForm = document.querySelector('.feedback > form');
 
 
+feedBackForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let input = feedBackForm.children[0].value;
 
+  alert(input);
+  feedBackForm.reset();
+});
 
 
